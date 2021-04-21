6fcdeceae4778ca1b617ddc77b8cf018c456e792
chetengchenwei <chetengchenwei@didiglobal.com>
2021-04-21 15:23:50 +0800

    add in travis-A

---
+++ src/main/java/com/didichuxing/travislearning/TravisLearningApplication.java
@@ -0,0 +1,13 @@
+package com.didichuxing.travislearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisLearningApplication.class, args);
+    }
+
+}
